399b418b63e25bd788cc13173701a5140ca9b0e6
Kadyke <yndx-petrov-dmit@yandex.ru>
2022-10-27 08:02:46 +0400

    style: изменить название переменной j на progress

--- src/Epic.java
+++ src/Epic.java
@@ -5,17 +5,17 @@ public class Epic extends Task {
     protected ArrayList<Integer> subtasksId = new ArrayList<>();
 
     public void setStatus() {
-        int j = 0;
+        int progress = 0;
         for (Integer integer : subtasksId) {
             if (Objects.equals(TasksManager.subtasks.get(integer).getStatus(), "DONE")) {
-                j = j +2;
+                progress = progress + 2;
             } else if (Objects.equals(TasksManager.subtasks.get(integer).getStatus(), "IN_PROGRESS")) {
-                j = j +1;
+                progress = progress + 1;
             }
         }
-        if (j == (subtasksId.size() * 2)) {
+        if (progress == (subtasksId.size() * 2)) {
             this.status = "DONE";
-        } else if (j == 0) {
+        } else if (progress == 0) {
             this.status = "NEW";
         } else {
             this.status = "IN_PROGRESS";
